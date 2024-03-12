909c122d6943a105355ada4d6228378ff59d9324
Abhijeet Kawhale <abhijeet.kawhale@gmail.com>
2024-03-12 11:39:25 +0530

    function and string
    Function and String

---
+++ 03_funAssign02.js
@@ -0,0 +1,7 @@
+function bankDetail(bankName,accNum,location,pinCode){
+    console.log("Bank Name :", bankName, "  " ,"Acc Number :", accNum, "  " ,"City :",location, "  ", "PINCODE :", pinCode);
+ 
+}
+bankDetail("CITI Bank",4563746876,"Pune",431202);
+bankDetail("AXIS Bank",6785769876,"Nashik",987865);
+bankDetail("HDFC Bank",6758675879,"Satara",712123);
---
+++ 03_stringBasicsAssig01.js
@@ -0,0 +1,15 @@
+console.log("==================Assignment 01======================")
+
+function stringBasics(){
+    var company="Microsoft";
+    console.log("My Dream Company is=",company);
+}
+stringBasics();
+
+var hobby1="Coding";
+var hobby2="Singing Song";
+var hobby3="Playing Cricket";
+console.log(`My Hobbies Are = ${hobby1}, ${hobby2}, ${hobby3}`);
+var totalChar=hobby1.length + hobby2.length + hobby3.length;
+console.log(`Sum the Total Number of Characters is= ${totalChar}`);
+
